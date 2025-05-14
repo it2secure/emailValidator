3ab5f117ac1928bf5ac60020029d7dcbe1b79364
it2secure <100015454+it2secure@users.noreply.github.com>
2025-05-14 12:38:18 +0330

    Add files via upload

---
+++ email.py
@@ -0,0 +1,11 @@
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+      print("Valid email")
+    else:
+      print("Invalid email")
+res=input('Enter your email address for validating: ')
+isValid(res)
